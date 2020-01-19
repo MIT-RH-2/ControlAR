2ca127653d8a47dff5586a7ed4747ec336a2ae6f
tomthecarrot <code@tomthecarrot.com>
2020-01-19 10:59:12 -0500

    Add ControlArModel logic

---
+++ Assets/ControlArModel.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Teleportal;
+
+public class ControlArModel : MonoBehaviour
+{
+    
+    // Singleton reference //
+    public static ControlArModel Shared;
+
+    [SerializeField]
+    private float RotationSendInterval = 0.25f;
+
+    [SerializeField]
+    private float SmoothingTime = 0.25f;
+
+
+    // tldr; stream and return phoneVect (euclidean angles) and ignore the x direction, read chat to see why
+
+    // short answer is that the orientation requested by Weston means that the x direction is completely arbitrary with respect to inital
+    // bootup of the app. this has to happen in one of them and it is x in ours. So just use y and z rotation to manipulate the paper airplane.
+    // I'll send a screenshot showing what those rotations are in real life. This package is good to be downloaded onto an android and run, and the UI is passable haha although
+    // Weston can look over it if he likes. The variable ' phoneVect ' is what you want to stream if you want euclidean. return phoneQuat if you want Quaternion
+    private Quaternion phoneQuat;
+    private Vector3 phoneVect;
+    private Quaternion TargetRot;
+
+    private TPObject tpo;
+
+    [SerializeField]
+    private InputField inputField;
+
+    ///// LIFECYCLE /////
+
+    void Awake() {
+        this.tpo = TPObject.get(this);
+    }
+
+    void Start() {
+        // Subscribe listener (all platforms -- desktop, mobile, Magic Leap)
+        this.tpo.Subscribe("accel", this.TransformObject);
+
+        // Start sending accelerometer data (iOS/Android only)
+        if (Teleportal.TPDeviceInfo.IsMobile) {
+            this.tpo.locked = true; // local->server transform lock
+            Input.gyro.enabled = true;
+            this.StartCoroutine(this.SendAccelRepeated(this.RotationSendInterval));
+        }
+        
+        // Login automatically (Magic Leap / desktop only)
+        else {
+            this.StartCoroutine(this.AutoLogin());
+        }
+
+        // Start object motion
+        StartCoroutine(this.SmoothTransform());
+        //this.ApplyContinuousForce(); // now handled by Constant Force behaviour (see Inspector GUI)
+    }
+
+    ///// DEVICE MOTION /////
+
+    private IEnumerator AutoLogin() {
+        // Wait for connection to Teleportal network
+        while (!Teleportal.Teleportal.tp.IsConnected()) {
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        this.inputField.text = "mitrealityhack-ML";
+        this.inputField.OnSubmit(null);
+    }
+
+    private IEnumerator SendAccelRepeated(float interval) {
+        Quaternion deviceQuat;
+        Vector3 deviceEuler;
+        
+        // Wait for connection to Teleportal network
+        while (!Teleportal.Teleportal.tp.IsConnected()) {
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        while (Teleportal.Teleportal.tp.IsConnected()) {
+            // Get rotation
+            deviceQuat = Input.gyro.attitude;
+            deviceQuat = this.GyroToUnity(deviceQuat);
+            deviceEuler = deviceQuat.eulerAngles;
+
+            // Send via network
+            string deviceStr = deviceQuat.x + "," + deviceQuat.y + "," + deviceQuat.z + "," + deviceQuat.w;
+            tpo.SetState("accel", deviceStr);
+            Debug.Log("SEND: " + deviceStr);
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    // from https://docs.unity3d.com/ScriptReference/Gyroscope.html //
+    private Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+
+    ///// MODEL ANIMATION /////
+
+    public void TransformObject(string transformStr) {
+        string[] arr = transformStr.Split(',');
+        float rotX = float.Parse(arr[0]);
+        float rotY = float.Parse(arr[1]);
+        float rotZ = float.Parse(arr[2]);
+        float rotW = float.Parse(arr[3]);
+
+        this.TargetRot = new Quaternion(-rotX, rotZ, rotY, rotW);
+        this.TargetRot = this.GyroToUnity(this.TargetRot);
+    }
+
+    private IEnumerator SmoothTransform() {
+        while (true) {
+
+            float t = 0f;
+            while (t < this.SmoothingTime) {
+                this.transform.rotation = Quaternion.Slerp(
+                    this.transform.rotation,
+                    this.TargetRot,
+                    t / this.SmoothingTime
+                );
+                // Debug.Log(t);
+                // Debug.Log(this.transform.eulerAngles);
+                t += Time.deltaTime;
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+
+        }
+    }
+
+    private void ApplyContinuousForce() {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        rb.AddForce(this.transform.forward, ForceMode.Force);
+    }
+
+}
--- Assets/Scripts/RgbNet.cs
+++ Assets/Scripts/RgbNet.cs
@@ -20,14 +20,6 @@ public class RgbNet : MonoBehaviour
 
     [SerializeField]
     private int Port = 8001;
-    
-    [SerializeField]
-    private float SmoothingTime = 0.25f;
-
-    [SerializeField]
-    private GameObject TargetObject;
-
-    private Vector3 TargetAngles;
 
     private WebSocket ws;
     private UdpClient udp;
@@ -70,26 +62,12 @@ public class RgbNet : MonoBehaviour
     }
 
     void Start() {
-        // Null checks
-        if (this.TargetObject == null) {
-            Debug.LogError("Target object is null! Please assign it in the Inspector GUI.");
-        }
-
-        if (this.TargetObject.GetComponent<Rigidbody>() == null) {
-            Debug.LogError("Target object does not have a Rigidbody attached. " +
-                "Please attach the Rigidbody component in the Inspector GUI.");
-        }
-
         // Start networking
         StartCoroutine(this.openWs());
         this.openUdp();
 
         // Test networking
         //StartCoroutine(this.testWsUdp());
-
-        // Start object motion
-        // this.ApplyContinuousForce();
-        //StartCoroutine(this.SmoothTransform());
     }
 
     ///// NETWORKING /////
@@ -98,8 +76,8 @@ public class RgbNet : MonoBehaviour
         // this.sendWs("test");
         while (true) {
             this.sendUdp("test");
-            //Debug.Log("UDP sent");
-            yield return new WaitForSeconds(this.SmoothingTime);
+            Debug.Log("UDP sent");
+            yield return new WaitForSeconds(0.25f);
         }
     }
 
@@ -115,7 +93,7 @@ public class RgbNet : MonoBehaviour
             string msg = this.ws.RecvString();
             if (msg != null) {
                 Debug.Log("WS recv: " + msg);
-                this.TransformObject(msg);
+                ControlArModel.Shared.TransformObject(msg);
             }
 
             yield return new WaitForSeconds(0.01f);
@@ -161,41 +139,6 @@ public class RgbNet : MonoBehaviour
             this.udp.Send(b, b.Length);
         } catch (Exception e) {
             Debug.LogError(e.Message);
-        }
-    }
-
-    ///// TRANSFORM & ANIMATION /////
-
-    private void ApplyContinuousForce() {
-        Rigidbody rb = this.TargetObject.GetComponent<Rigidbody>();
-        rb.AddForce(this.TargetObject.transform.forward, ForceMode.Force);
-    }
-
-    private void TransformObject(string transformStr) {
-        string[] arr = transformStr.Split(',');
-        float rotX = float.Parse(arr[0]);
-        float rotY = float.Parse(arr[1]);
-        float rotZ = float.Parse(arr[2]);
-
-        this.TargetAngles = new Vector3(rotX, rotY, rotZ);
-    }
-
-    private IEnumerator SmoothTransform() {
-        while (this.TargetObject != null) {
-
-            float t = 0f;
-            while (t < this.SmoothingTime) {
-                this.TargetObject.transform.eulerAngles = Vector3.Slerp(
-                    this.TargetObject.transform.eulerAngles,
-                    this.TargetAngles,
-                    t / this.SmoothingTime
-                );
-                Debug.Log(t);
-                Debug.Log(this.TargetObject.transform.eulerAngles);
-                t += Time.deltaTime;
-                yield return new WaitForSeconds(Time.deltaTime);
-            }
-
         }
     }
 }
